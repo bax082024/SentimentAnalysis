c0599b8d6890d0ae37316561e6acb5c0bcc56396
bax082024 <bax082024@gmail.com>
2024-11-05 22:00:57 +0100

    fixed syntax errors

--- Program.cs
+++ Program.cs
@@ -4,45 +4,46 @@ using Microsoft.ML.Data;
 
 class Program
 {
-  static void Main(string[] args)
-  {
-    MLContext mLContext = new MLContext;
-  }
-
-  static IDataView LoadData(MLContext mlContext)
-  {
-    var trainingData = new[]
+    static void Main(string[] args)
     {
-      new SentimentData { Text = "This product is great!", Label = true},
-      new SentimentData { Text = "i did not like this product at all", Label = false},
-      new SentimentData { Text = "This is the best thing i have ever bought!", Label = true},
-      new SentimentData { Text = "Worst purchase ever"}
-    };
-  }
+        MLContext mlContext = new MLContext();
+
+        var dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText("Features", nameof(SentimentData.Text));
+        var trainer = mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features");
+        var trainingPipeline = dataProcessPipeline.Append(trainer);
+
+        var model = trainingPipeline.Fit(LoadData(mlContext));
+    }
+
+    static IDataView LoadData(MLContext mlContext)
+    {
+        var trainingData = new[]
+        {
+            new SentimentData { Text = "This product is great!", Label = true },
+            new SentimentData { Text = "I did not like this product at all.", Label = false },
+            new SentimentData { Text = "This is the best thing I have ever bought.", Label = true },
+            new SentimentData { Text = "Worst purchase ever", Label = false }
+        };
+
+        return mlContext.Data.LoadFromEnumerable(trainingData);
+    }
 }
-
-
-
-
-
-
-
-
 
 public class SentimentData
 {
-  [LoadColumn(0)] // Text Collumn
-  public string Text { get; set;}
+    [LoadColumn(0)] 
+    public string? Text { get; set; }
 
-  [LoadColumn(1)] // True / False Collumn
-  public bool Label { get; set; } 
-
+    [LoadColumn(1)] 
+    public bool Label { get; set; } 
 }
 
 public class SentimentPrediction : SentimentData
 {
-  [ColumnName("PredictedLabel")]
-  public bool Prediction { get; set; }
-  public float Probability { get; set; }
-  public float Score { get; set; }
+    [ColumnName("PredictedLabel")]
+    public bool Prediction { get; set; }
+    
+    public float Probability { get; set; }
+    
+    public float Score { get; set; }
 }
--- obj/Debug/net8.0/SentimentAnalysis.AssemblyInfo.cs
+++ obj/Debug/net8.0/SentimentAnalysis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SentimentAnalysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f177df44447e350279fdf7f897e2aa5c775603d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a534c0ff32b10bd08d243e3d910a0150b48ea81")]
 [assembly: System.Reflection.AssemblyProductAttribute("SentimentAnalysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SentimentAnalysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
